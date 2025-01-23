2fd7a40b1ef0d794693a63c973ae1777a241851c
Jacek programmer <jacek301@gmail.com>
2025-01-23 01:36:09 +0100

    Write text in component

--- src/MainScrren/MainScrren.tsx
+++ src/MainScrren/MainScrren.tsx
@@ -12,25 +12,20 @@ const MainScrren = () => {
       <h1 className='text-3xl font-bold underline text-yellow-500 text-center'>
         Wpisuj swój tekst
       </h1>
-      <div className='flex flex-wrap  justify-start  items-center  p-2 '>
-        <TextPlace content="fog in the bay Golden Bough lala sddw wdwdwdwdwdwdwdwdwdwdwd" />
-        <TextPlace content="drugi element wiekszy element lalala dwa ereere eefefeefefefefefefeffefefwef" />
-        <TextPlace content="trzeci element" />
-        <TextPlace content="trzeci element" />
-        <TextPlace content="trzeci element" />
-        <TextPlace content="trzeci element" />
-        <TextPlace content="element" />
-        <TextPlace content="trzt" />
-        <TextPlace content="trzt" />
-        <TextPlace content="trzt" />
-        <TextPlace content="trzt" />
-        <TextPlace content="trzt" />
-        <TextPlace content="trzt" />
-        <TextPlace content="trzt" />
-        <TextPlace content="trzt" />
-        <TextPlace content="trzt" />
-        <TextPlace content="trzt" />
-        <TextPlace content="trzt" />
+      <div className='flex flex-wrap justify-start items-start p-2'>
+        <TextPlace  />
+        <TextPlace  />
+        <TextPlace  />
+        <TextPlace  />
+        <TextPlace  />
+        <TextPlace  />
+        <TextPlace  />
+        <TextPlace  />
+        <TextPlace  />
+        <TextPlace  />
+        <TextPlace  />
+        <TextPlace  />
+        <TextPlace  />
       </div>
     </div>
 
--- src/TextPlace/TextPlace.tsx
+++ src/TextPlace/TextPlace.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { RefObject, useEffect, useRef, useState } from 'react';
 
-interface TextContent {
-  content: string;
-}
+const TextPlace = () => {
 
-const TextPlace: React.FC<TextContent> = (props) => {
+  const [inputText, setText] = useState('');
+  const textareaRef: RefObject<HTMLTextAreaElement> = useRef(null);
+
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setText(event.target.value);
+  };
+
+  useEffect( () => {
+    if(textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  });
 
   return (
-    <div className="w-36 rounded  shadow-lg bg-green-700 p-2
+    <div className="w-36 rounded  shadow-lg bg-green-700
      text-white border border-yellow-500
-     hover:bg-green-950 hover:shadow-2xl min-w-[50px] min-h-[50px] m-1 break-words">
-      {props.content}
+     hover:bg-green-950 hover:shadow-2xl min-w-[100px] min-h-[50px] m-0 break-words">
+      <textarea
+        ref={textareaRef}
+        value={inputText}
+        onChange={handleChange}
+        className='resize-none w-full bg-inherit overflow-hidden'
+      />
     </div>
   )
 }
