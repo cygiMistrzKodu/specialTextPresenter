06c0f45efa2cb8a1f7f219bbe6d6d8efc6b9ac19
Jacek programmer <jacek301@gmail.com>
2025-02-27 00:58:55 +0100

    Cleaning: remove dead code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,28 +3,16 @@ pub mod autosave;
 use autosave::Task;
 use std::env;
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            greet,
-            write_container_content,
             auto_save_tasks,
             read_saved_tasks_contents
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-#[tauri::command]
-fn write_container_content(task_content: &str) {
-    print!("Z fontu ->>: {}\n", task_content)
 }
 
 #[tauri::command]
